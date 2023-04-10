215233b5577751ae7e92c377c7a10c97c85d2cbf
Andreas Job <andreas.job@gmx.de>
2023-04-10 13:19:32 +0200

    Initial Commit

---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,20 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Andreas Job on 10.04.23.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
