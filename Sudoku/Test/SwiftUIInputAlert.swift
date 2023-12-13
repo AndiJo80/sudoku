4db0193e1c32f7e025c728393f589365cfe19cbe
Andreas Job <andreas.job@gmx.de>
2023-12-13 13:58:25 +0100

    change user name

---
+++ Sudoku/Test/SwiftUIInputAlert.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIInputAlert.swift
+//  Sudoku
+//
+//  Created by Andreas Job on 10.12.23.
+//
+
+import SwiftUI
+
+struct SwiftUIInputAlert: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIInputAlert()
+}
